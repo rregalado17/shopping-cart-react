ccd2ad05d68cd44109f0e10a718cf15ac15d8cee
rregalado17 <rregalado17@gmail.com>
2021-07-28 22:55:18 -0400

    shopping cart updates react' '

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,24 @@ import itemData from "../data/items";
 
 function App() {
   const [items, setItems] = useState(itemData);
+  const [isDarkMode, setDarkMode] = useState(false)
 
   // this data will be passed down to the ShoppingList as a prop
   console.log(items);
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  // const appClass = false ? "App dark" : "App light"
+
+  function handleDarkMode() {
+    setDarkMode((isDarkMode) => !isDarkMode)
+  }
 
   return (
-    <div className={appClass}>
+    <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleDarkMode}>{isDarkMode ? 'Dark' : 'Light'}</button>
       </header>
       <ShoppingList items={items} />
     </div>
